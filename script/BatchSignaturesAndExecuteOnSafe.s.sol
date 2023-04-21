1f14f66759227fc3feeb468bc226dc2315ce30d1
MerlinEgalite <egalite.merlin@gmail.com>
2023-04-21 09:34:25 +0200

    feat: build tx and execute it

--- script/BatchSignaturesAndExecuteOnSafe.s.sol
+++ script/BatchSignaturesAndExecuteOnSafe.s.sol
@@ -6,32 +6,60 @@ import "forge-std/console2.sol";
 import "safe/GnosisSafe.sol";
 
 contract BatchSignaturesAndExecuteOnSafe is Script {
+    // Avoid stack too deep error.
+    struct TxData {
+        address to;
+        uint256 value;
+        bytes data;
+        Enum.Operation operation;
+        uint256 safeTxGas;
+        uint256 baseGas;
+        uint256 gasPrice;
+        address gasToken;
+        address payable refundReceiver;
+        bytes signatures;
+    }
 
     function run() public {
-        uint256 nbOfSignatures = 2; // TODO: automatically fetch the number of signatures?
-        bytes memory signatures;
-
         string memory root = vm.projectRoot();
         string memory path = string.concat(root, "/signatures/");
 
+        uint256 nbOfSignatures = 2; // TODO: automatically fetch the number of signatures?
+        TxData memory txData;
+
+        // Build signatures payload.
         for (uint256 i; i < nbOfSignatures; i++) {
-            signatures = bytes.concat(signatures, vm.parseBytes(vm.readFile(string.concat(path, vm.toString(i), ".txt"))));
+            txData.signatures =
+                bytes.concat(txData.signatures, vm.parseBytes(vm.readFile(string.concat(path, vm.toString(i), ".txt"))));
         }
 
-        /*
+        // Fetch Safe contract.
         GnosisSafe safe = GnosisSafe(payable(vm.envAddress("SAFE")));
-        address to; // Multicall?
-        uint256 value; // Keep 0?
-        bytes memory data; // To fetch.
-        Enum.Operation operation; // Call or delegatecall?
-        uint256 safeTxGas; // To fetch.
-        uint256 baseGas; // To fetch.
-        uint256 gasPrice; // Gas price
-        address gasToken = address(0); // ETH
-        address payable refundReceiver = payable(address(0)); // tx.origin
 
-        vm.broadcast();
-        safe.execTransaction(to, value, data, operation, safeTxGas, baseGas, gasPrice, gasToken, refundReceiver, signatures);
-        */
+        // Build tx data.
+        txData.to = vm.envAddress("TO");
+        txData.value = 0;
+        txData.data = vm.parseBytes(vm.readFile(string.concat(path, "data.txt")));
+        txData.operation = Enum.Operation.Call;
+        txData.safeTxGas = 0;
+        txData.baseGas = 0;
+        txData.gasPrice = 0;
+        txData.gasToken = address(0); // ETH
+        txData.refundReceiver = payable(address(0)); // txData.origin
+
+        // Execute tx.
+        vm.broadcast(vm.envAddress("SENDER"));
+        safe.execTransaction(
+            txData.to,
+            txData.value,
+            txData.data,
+            txData.operation,
+            txData.safeTxGas,
+            txData.baseGas,
+            txData.gasPrice,
+            txData.gasToken,
+            txData.refundReceiver,
+            txData.signatures
+        );
     }
 }
